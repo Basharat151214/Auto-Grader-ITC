a74c2eacafcc24be69b819d892236ba13c788f82
Basharat Hussain <31223223+Basharat151214@users.noreply.github.com>
2020-03-04 21:12:33 +0500

    Assignment

---
+++ a02.py
@@ -0,0 +1,15 @@
+## IMPORTS GO HERE if required
+
+## END OF IMPORTS 
+
+
+#### YOUR CODE FOR netIncome GOES HERE ####
+def netIncome(x,y=0.02):
+	netsalary = (x)-(x)*(2/100.0)
+	return netsalary
+
+
+
+#### End OF MARKER
+
+
